eda0d065ac16a8822cfa7644f12ecaa1ddc0da38
Kyosuke Takayama <loiseau@gmail.com>
2018-07-05 23:01:56 +0900

    Initial Commit

---
+++ DummyEventCreater/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DummyEventCreater
+//
+//  Created by Kyosuke Takayama on 2018/07/05.
+//  Copyright © 2018年 Kyosuke Takayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
